9069f7e63cdda4a7c5b596ba2dbd2881e420d5bf
xiaojianrui <xiaojianrui>
2018-12-25 17:25:58 +0800

    备注

--- src/components/community/Academic.vue
+++ src/components/community/Academic.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <ul>
-        <router-link 
+        <router-link
         v-for ="item in questionList"
         :key = "item.questionId"
         tag= "li"
@@ -15,14 +15,15 @@
 <script>
 
 export default{
-    beforeRouteEnter(to,from,next){
-        const answer = confirm('还没有登陆，要登陆吗');
-        if(answer){
-            next({name:'Person'});
-        }else{
-            next(false);
-        }
-    },
+    // 组件内守卫
+    // beforeRouteEnter(to,from,next){
+    //     const answer = confirm('还没有登陆，要登陆吗');
+    //     if(answer){
+    //         next({name:'Person'});
+    //     }else{
+    //         next(false);
+    //     }
+    // },
     data(){
         return {
             questionList: [
